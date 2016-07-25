548b204cc9f1f1a76291b78a45d5d82c68d3593d
Charter Jacobson <me@katacarbix.xyz>
2016-07-25 18:59:24 -0500

    some code fixing
    just modified your testing code a little. print isn't a function so
    using parentheses makes it interpret inputs as tuples.

--- owensPythonLearning/PlayingWithDataStructures.py
+++ owensPythonLearning/PlayingWithDataStructures.py
@@ -2,34 +2,34 @@ from collections import deque
 import PackageTest.OwensModule as owenMod
 
 a = list(range(10))
-print(a)
+print a
 
 a.append(11)
-print(a)
+print a
 
 a.extend(a)
-print(a)
-print(a.count(2))
+print a
+print a.count(2)
 a.reverse()
-print(a)
+print a
 
 a[1] = 200 #Lists are MUTABLE ('think of this as mutatable'); they can be changed after creation
-print(a)
+print a
 
 queue = deque(a) #Make a deque from 'a'
-print(queue.popleft())
-print(queue)
+print queue.popleft()
+print queue
 
 def isEven(x): return x % 2 is 0
 
-print(list(filter(isEven, range(20)))) #get the values from range(20) that satisfy isEven
-print(list(map(isEven, range(20)))) #Map isEven to the list range and print the list of return values
+print list(filter(isEven, range(20))) #get the values from range(20) that satisfy isEven
+print list(map(isEven, range(20))) #Map isEven to the list range and print the list of return values
 
 def squares(n):
 	return [x ** 2 for x in range(n)] #Uses a "list comprehension" to define a list of squares.
 	#This would also work here: return map(lambda x: x **2, range(n))
 
-print(squares(5)) #print the first five square numbers
+print squares(5) #print the first five square numbers
 
 def oddFilter(L): #Manually filters out odd numbers. Of course, the filter method would work better here, but I wanted to do it with a list comprehension
 	return [x for x in L if not isEven(x)]
@@ -42,60 +42,60 @@ matrix = [
 	[7, 8, 9]
 ]
 
-print(matrix, len(matrix))
-print(matrix[2])
+print matrix, len(matrix)
+print matrix[2]
 
 def transpose(M):
 	return [[row[i] for row in M] for i in range(len(M))] #Nested list comprehensions transposing a matrix. Inner nest corresponds to the value of each COLUMN in the output matrix.
 	#Apparently zip(*matrix) would work here as well. zip() is basically THE function for transposition. Just remember the * to unpack the matrix into arguments for zip()
-print(transpose(matrix))
+print transpose(matrix)
 
 b = list(range(10))
 del b[2:5] #delete elements 2, 3, and 4 from b. Does NOT delete element 5
-print(b)
+print b
 del b
 #print(b, a) would error because del b completely deletes the reference to b. b becomes undefined
 
 #TUPLES:
 t = 5, 2, 'hi'
-print(t)
-print(*t) #Unpacking the tuple, prints '5 2 hi' instead of '(5, 2, 'hi')'
+print t
+print (*t) #Unpacking the tuple, prints '5 2 hi' instead of '(5, 2, 'hi')'
 var1, var2, var3 = t
-print(var1, var2, var3) #'Sequence unpacking' - multiple variable assignments in one! Yay!
+print var1, var2, var3 #'Sequence unpacking' - multiple variable assignments in one! Yay!
 #t[0] = 3 #THIS DOES NOT WORK! TUPLES ARE IMMUTABLE
 
 #SETS:
 letters = ['a', 'b', 'a', 'c', 'c', 'e']
 letterSet = set(letters)
-print(letterSet)
+print letterSet
 
 hasLetters = 'd' in letterSet, 'a' in letterSet #sets have quick ways of checking membership
-print(hasLetters)
+print hasLetters
 
 del a
 a = {x for x in 'abracadabra' if x not in 'abc'} #Set comprehensions work exactly like list comprehensions do
-print(a)
+print a
 
 #DICTIONARIES (Basically key-value pairs)
 robots = {'2175-2013' : 'Trap', '2175-2014' : 'Atlas', '2175-2015' : 'Melman', '2175-2016' : 'Night Fury'}
-print(robots['2175-2014'])
+print robots['2175-2014']
 robots.pop('2175-2015') #It was a bad year. We don't want it in our dictionary...
-print(robots.keys())
+print robots.keys()
 
 dictComp = {x: x + 5 for x in range(5)} #Dictionaries have comprehensions too
-print(dictComp)
+print dictComp
 
 aList = ['one', 'two', 'three', 'four']
 for i, v in enumerate(aList):
-	print('the index is {0} and the value is {1}'.format(i, v))
+	print 'the index is {0} and the value is {1}'.format(i, v)
 
 for f in sorted(aList): #Sorted does not change the original list, unlike "sort()"
-	print(f)
+	print f
 
 for f in reversed(aList):
-	print(f)
+	print f
 
 if([1, 2, 3] > [1, 1, 100]): #Lists can be compared, but they are compared lexicographically, as in this example here.
-	print('Yes')
+	print 'Yes'
 
-print(owenMod.appendOwenToString('Who\'s a genius programmer? it\'s '))
+print owenMod.appendOwenToString('Who\'s a genius programmer? it\'s ')
--- owensPythonLearning/PlayingWithStrings.py
+++ owensPythonLearning/PlayingWithStrings.py
@@ -1,9 +1,9 @@
-print("""\
+print """
 This is a test
 of multiple line printing
-in python""" )
+in python"""
 
-print(3 * 'un' + 'ium')
+print 3 * 'un' + 'ium'
 
 word = 'pythonz'
 for i in range(len(word)):
@@ -12,16 +12,16 @@ for i in range(len(word)):
 def wordContainsZ(wordToCheck):
 	for j in range(len(wordToCheck)):
 		if wordToCheck[j] is 'Z' or wordToCheck[j] is 'z':
-			print('The word', wordToCheck, 'contains a z or Z')
+			print 'The word', wordToCheck, 'contains a z or Z'
 			break
 	else:
-		print('The word', wordToCheck, 'does not contain a z or Z')
+		print 'The word', wordToCheck, 'does not contain a z or Z'
 
 def betterWordContainsZ(wordToCheck):
 	if 'Z' in word or 'z' in word:
-		print('The word', wordToCheck, 'contains a z or Z')
+		print 'The word', wordToCheck, 'contains a z or Z'
 	else:
-		print('The word', wordToCheck, 'does not contain a z or Z')
+		print 'The word', wordToCheck, 'does not contain a z or Z'
 
 wordContainsZ(word)
 betterWordContainsZ(word)
